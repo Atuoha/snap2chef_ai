29811f45ac489ffb367ab32d370628e65b8fb2ad
Atuoha Anthony <atuohainitiatives@gmail.com>
2025-05-24 07:44:31 +0100

    Update app_strings.dart

--- lib/core/constants/app_strings.dart
+++ lib/core/constants/app_strings.dart
@@ -6,7 +6,7 @@ class AppStrings {
   static const String APP_SUBTITLE =  "Capture a photo or use your voice to get step-by-step guidance on how to prepare your favorite dishes or snacks";
   static const String APP_TITLE = "Your Personal AI Recipe Guide";
 
-  static const String AI_TEXT_PART = "You are a recipe ai expert. Generate a recipe based on this image, include recipe name, preparation steps, and a public YouTube video demonstrating the preparation step."
+  static const String AI_TEXT_PART = "You are a recipe ai expert. Generate a recipe based on this image, include recipe name, preparation steps, and a public YouTube video demonstrating the preparation step. Output the YouTube video URL on a new line prefixed with 'YouTube Video URL: ', it should be a https URL and the image URL on a new line prefixed with 'Image URL: ' and it should be a https URL too."
       "If the image is not a food, snacks or drink, politely inform the user that you can only answer recipe queries and ask them to close and upload a food/snack/drink image.";
 
   static const String AI_AUDIO_PART =
